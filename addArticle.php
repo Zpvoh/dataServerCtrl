f42245a2127b91569a378f558239480b32d832b9
zpvoh <16302010023@fudan.edu.cn>
2018-07-23 23:56:31 +0800

    Initial commit

---
+++ addArticle.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zpvoh
+ * Date: 18-7-22
+ * Time: 下午9:40
+ */
+
+
+$host='localhost:3306';
+$user='root';
+$pass='82870808qyy';
+$db=new mysqli($host, $user, $pass, "articles");
+if($db->connect_errno){
+    echo "There is no such database.";
+}
+
+$query="insert into `articleNames` (`name`, `theme`) values ('{$_POST['name']}', '{$_POST['theme']}')";
+$result=$db->query($query);
+
+$db->close();
